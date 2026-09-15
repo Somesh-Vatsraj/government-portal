47d725f0f038239988142e813b42003a1f2b4579
Somesh Vatsraj <someshsoftwareengineer@gmail.com>
2026-09-15 19:33:08 +0530

    Create CreatePost.jsx

---
+++ src/admin/CreatePost.jsx
@@ -0,0 +1,10 @@
+import PostForm from './PostForm.jsx';
+
+export default function CreatePost() {
+  return (
+    <>
+      <h1 className="admin-h1">Create Post</h1>
+      <PostForm />
+    </>
+  );
+}
