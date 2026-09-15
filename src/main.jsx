1293d5afbab37e8f056e7a6710b35e89a9ac9f78
Somesh Vatsraj <someshsoftwareengineer@gmail.com>
2026-09-15 19:20:58 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './styles/global.css';
+import './styles/header.css';
+import './styles/cards.css';
+import './styles/post.css';
+import './styles/admin.css';
+import './styles/responsive.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
